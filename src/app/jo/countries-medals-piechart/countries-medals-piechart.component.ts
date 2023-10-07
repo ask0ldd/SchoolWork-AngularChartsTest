6e6c20b796a5bc81809af4d335e0a2add1c6042b
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-10-07 13:20:37 +0200

    upd angular default settings + components

--- src/app/jo/countries-medals-piechart/countries-medals-piechart.component.ts
+++ src/app/jo/countries-medals-piechart/countries-medals-piechart.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countries-medals-piechart',
-  template: `
-    <p>
-      countries-medals-piechart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './countries-medals-piechart.component.html',
+  styleUrls: ['./countries-medals-piechart.component.css']
 })
 export class CountriesMedalsPiechartComponent {
 
--- src/app/jo/country-stats-linechart/country-stats-linechart.component.ts
+++ src/app/jo/country-stats-linechart/country-stats-linechart.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-country-stats-linechart',
-  template: `
-    <p>
-      country-stats-linechart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './country-stats-linechart.component.html',
+  styleUrls: ['./country-stats-linechart.component.css']
 })
 export class CountryStatsLinechartComponent {
 
