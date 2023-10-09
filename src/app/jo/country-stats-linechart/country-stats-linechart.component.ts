b8e43159f9103d477b52fedae8841c000f835673
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-10-09 21:47:21 +0200

    linechart page layout

--- src/app/jo/country-stats-linechart/country-stats-linechart.component.ts
+++ src/app/jo/country-stats-linechart/country-stats-linechart.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { JoMockapiService } from '../jo-mockapi.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-country-stats-linechart',
   templateUrl: './country-stats-linechart.component.html',
-  styleUrls: ['./country-stats-linechart.component.css']
+  styleUrls: ['./country-stats-linechart.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CountryStatsLinechartComponent implements OnInit {
 
